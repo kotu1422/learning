5e311e1d3fd80cdccaabbd8d93ae6e0e5b4d003f
kotu1422 <118930566+kotu1422@users.noreply.github.com>
2022-12-08 20:34:34 +0100

    11th codewars task with others' ideas

--- Codewars/dig_pow.py
+++ Codewars/dig_pow.py
@@ -36,3 +36,18 @@ dig_pow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
 dig_pow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 dig_pow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51"""
+#################
+"""
+not mine:
+def dig_pow(n, p):
+  s = 0
+  for i,c in enumerate(str(n)):
+     s += pow(int(c),p+i)
+  return s/n if s%n==0 else -1
+
+
+def dig_pow(n, p):
+    k, fail = divmod(sum(int(d)**(p + i) for i, d in enumerate(str(n))), n)
+    return -1 if fail else k
+
+"""
