caa59888c99b8f28e8aaa71e99803f00d2d70c53
kotu1422 <118930566+kotu1422@users.noreply.github.com>
2022-12-11 18:55:56 +0100

    setup

---
+++ CodeGymTasks/1.JavaSyntax/src/en/codegym/task/pro/task05/task0504/Solution.java
@@ -0,0 +1,18 @@
+package en.codegym.task.pro.task05.task0504;
+
+/* 
+Combining arrays
+*/
+
+public class Solution {
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+        //write your code here
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + ", ");
+        }
+    }
+}
