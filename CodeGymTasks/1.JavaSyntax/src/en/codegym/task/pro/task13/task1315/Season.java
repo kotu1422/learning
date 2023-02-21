4137bc1181f3656b019139bcd10a5bf3caba9f5b
kotu1422 <118930566+kotu1422@users.noreply.github.com>
2023-02-21 20:21:15 +0100

    java learning

--- CodeGymTasks/1.JavaSyntax/src/en/codegym/task/pro/task13/task1315/Season.java
+++
@@ -1,2 +0,0 @@
-package en.codegym.task.pro.task13.task1315;public enum Season {
-}
--- CodeGymTasks/1.JavaSyntax/src/en/codegym/task/pro/task13/task1316/Solution.java
+++
@@ -1,8 +0,0 @@
-package en.codegym.task.pro.task13.task1316;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        //write your code here
-    }
-}
