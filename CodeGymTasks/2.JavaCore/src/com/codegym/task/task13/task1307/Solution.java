7c4b5a359ee2e3a2d29fe1e3a9ba4a1d4f6cff13
kotu1422 <118930566+kotu1422@users.noreply.github.com>
2023-03-06 16:58:21 +0100

    java learning in progress

---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task13/task1307/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task13.task1307;
+
+/* 
+Parameterized interface
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    class StringObject //write your code here
+    {
+    }
+}
---
+++ CodeGymTasks/2.JavaCore/src/com/codegym/task/task13/task1309/Solution.java
@@ -0,0 +1,11 @@
+package com.codegym.task.task13.task1309;
+
+/* 
+All that moves
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
